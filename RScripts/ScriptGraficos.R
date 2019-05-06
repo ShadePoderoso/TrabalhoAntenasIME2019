5c0888ffe2b1ae6eb59f718d6d2ab510d4244247
ShadePoderoso <caio_c@msn.com>
2019-05-06 10:39:59 -0300

    Gráficos com os resultados obtidos a partir do web plot digitizer e atualização do script com um rascunho

--- RScripts/ScriptGraficos.R
+++ RScripts/ScriptGraficos.R
@@ -1,5 +1,17 @@
-# Script para importar e plotar os gráficos de Rp/Ro
+# Script para calcular os erros
 
-G = function() {
-  
+Erro = function(v1, v2) {
+  E1 = abs(v1 - v2);
+  return(E1);
 }
+
+windows();
+plot(Table8,type='l',col='red', xlab='c/b', ylab='Rp/Ro', main='Gráfico Web Plot Digitizer', lwd=3);
+points(Table7,type='l',col='blue', lwd=3);
+points(Table6,type='l',col='green', lwd=3);
+points(Table5,type='l',col='cyan', lwd=3);
+points(Table4,type='l',col='black', lwd=3);
+points(Table3,type='l',col='yellow', lwd=3);
+points(Table2,type='l',col='purple', lwd=3);
+grid();
+legend('topright',legend=c('N = 8','N = 7','N = 6','N = 5','N = 4','N = 3','N = 2'), col=c('red','blue','green','cyan','black','yellow','purple'), lwd=3, bty = 'n')
