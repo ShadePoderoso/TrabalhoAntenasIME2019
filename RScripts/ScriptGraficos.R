2160cb255fa0b9fcd05f6fdb02457ae666156707
ShadePoderoso <caio_c@msn.com>
2019-05-06 11:56:24 -0300

    Erros
    Gráficos dos Erros das curvas

--- RScripts/ScriptGraficos.R
+++ RScripts/ScriptGraficos.R
@@ -15,3 +15,15 @@ points(Table3,type='l',col='yellow', lwd=3);
 points(Table2,type='l',col='purple', lwd=3);
 grid();
 legend('topright',legend=c('N = 8','N = 7','N = 6','N = 5','N = 4','N = 3','N = 2'), col=c('red','blue','green','cyan','black','yellow','purple'), lwd=3, bty = 'n')
+
+
+windows();
+plot(ITF8,type='l',col='red', xlab='c/b', ylab='Rp/Ro', main='Gráfico Interpolação da Tabela', lwd=3);
+points(ITF7,type='l',col='blue', lwd=3);
+points(ITF6,type='l',col='green', lwd=3);
+points(ITF5,type='l',col='cyan', lwd=3);
+points(ITF4,type='l',col='black', lwd=3);
+points(ITF3,type='l',col='yellow', lwd=3);
+points(ITF2,type='l',col='purple', lwd=3);
+grid();
+legend('topright',legend=c('N = 8','N = 7','N = 6','N = 5','N = 4','N = 3','N = 2'), col=c('red','blue','green','cyan','black','yellow','purple'), lwd=3, bty = 'n')
